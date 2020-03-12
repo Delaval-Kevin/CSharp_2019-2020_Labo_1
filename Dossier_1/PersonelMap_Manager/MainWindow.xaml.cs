4838abdcd8d3eaeb4c7306ef83f94e9825d85349
Delaval <delaval.k60@gmail.com>
2020-03-12 22:21:47 +0100

    :candy: Avancement sur l'interface

--- Dossier_1/PersonelMap_Manager/MainWindow.xaml.cs
+++ Dossier_1/PersonelMap_Manager/MainWindow.xaml.cs
@@ -15,19 +15,40 @@ using System.Windows.Shapes;
 
 namespace PersonelMap_Manager
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        IPageChange currentControl;
+
+
         public MainWindow()
         {
             InitializeComponent();
+            loadPageLogin();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void MainWindow_OnPageChange(int obj)
         {
-            this.Close();
+            switch (obj)
+            {
+                case 1: loadPagePrincipal(); break; // si on vient de la page 1 on va vers la page 2
+                case 2: loadPageLogin(); break; // si on vient de la page 2 on va vers la page 1
+            }
+        }
+
+        public void loadPagePrincipal()
+        {
+            currentControl = new PagePrincipal();
+            this.MonControle.Content = currentControl;
+
+            currentControl.pageChange += MainWindow_OnPageChange;
+        }
+
+        public void loadPageLogin()
+        {
+            currentControl = new PageLogin();
+            this.MonControle.Content = currentControl;
+
+            currentControl.pageChange += MainWindow_OnPageChange;
         }
     }
 }
